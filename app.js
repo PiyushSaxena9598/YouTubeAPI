8833d379131134e4b76aec2a4e1548fcf1dc952b
Piyush Saxena <piyushsaxena725@gmail.com>
2025-11-20 19:05:28 +0530

    minor change in app.js

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const userRoute=require('./routs/user');
-const videoRoute=require('./routs/video');
-const commentRoute=require('./routs/comment')
-const bodyParser=require('body-parser');
-const fileUpload=require('express-fileupload');
-const cors=require('cors')
-
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoute = require('./routs/user');
+const videoRoute = require('./routs/video');
+const commentRoute = require('./routs/comment');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 require('dotenv').config();
 
-
-// app.get('/test',(req,res)=>{
-//     res.status(200).json({message:'API is working fine'});
-// });
-
-const connetWithDB=async()=>{
-    try{
-        const res=await mongoose.connect(process.env.MONGO_URI);          
+// DB Connect
+const connectWithDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
-    }       
-    catch(err){
+    } catch (err) {
         console.log('Error connecting to MongoDB:', err);
     }
 };
-connetWithDB();
-app.use(cors())
-app.use(bodyParser.json());
+connectWithDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  
+
 app.use(fileUpload({
-    useTempFiles:true,
-    tempFileDir:'/tmp/'
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
 }));
-app.use('/user',userRoute);
-app.use('/video',videoRoute);
+
+// Routes
+app.use('/user', userRoute);
+app.use('/video', videoRoute);
 app.use('/comment', commentRoute);
 
-
-module.exports=app;
+module.exports = app;
